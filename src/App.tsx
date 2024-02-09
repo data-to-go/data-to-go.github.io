bffa8edb37b6c4101ba872677ad711317262f890
Aleksey Movchanyuk <aleksey.movchanyuk@dipocket.org>
2024-02-09 20:08:30 +0000

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DataToGo</h1>
+        <p>Your data, anytime, anywhere.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
